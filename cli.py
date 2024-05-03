861caa85e49fca6233e49a8a19d27217b2e90576
Michael Chirillo <m.chirillo@utexas.edu>
2024-05-03 11:31:05 +0200

    Make initial commit

---
+++ cli.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""Command line interface to interact with jser files."""
+
+import sys
+from pathlib import Path
+from funcs import *
+
+def print_example():
+
+    print("Example call: kh-cli <subcommand> <jser file>")
+    
+def print_usage():
+    
+    print(f"KH lab jser cli: {__doc__}")
+    print_example()
+
+def print_subcommands():
+    print("Possible subcommands include: objects, alignments, test")
+
+if __name__ == "__main__":
+
+    args = sys.argv
+
+    if len(args) == 1:
+        
+        print_usage()
+        sys.exit(0)
+
+    elif len(args) < 3:
+
+        print("Please provide at least 2 arguments to this command.")
+        print_example()
+        sys.exit(1)
+
+    # check if jser exists
+
+    jser = Path(args[2])
+
+    if not jser.exists():
+
+        print("Provided jser does not exist. Please provide a filepath to a valid jser file.")
+        print_example()
+        sys.exit(1)
+
+    data = load_jser(jser)
+
+    match args[1]:
+
+        case "objects":
+
+            print_objects(data)
+
+        case "alignments":
+
+            print_alignments(data)
+
+        case "test":
+            
+            print("test!")
+
+        case _:
+
+            print(f"Subcommand \"{args[1]}\" not recognized.")
+            print_subcommands()
+
---
+++ funcs.py
@@ -0,0 +1,39 @@
+"""Functions to interact with jser files."""
+
+
+import json
+
+
+def load_jser(filepath):
+
+    with open(filepath, "r") as fp:
+        data = json.load(fp)
+
+    return data
+
+
+def print_objects(data):
+
+    contours = set()
+
+    for section in data["sections"]:
+        for contour, _ in section["contours"].items():
+            contours.add(contour)
+
+    objects = list(contours)
+    objects.sort()
+
+    for obj in objects:
+        print(obj)
+
+def print_alignments(data):
+
+    current_alignment = data["series"]["alignment"]
+
+    tforms = data["sections"][0]["tforms"]
+    alignments = ["no-alignment"] + list(tforms.keys())
+
+    alignments = [e + "*" if e == current_alignment else e for e in alignments]
+
+    for alignment in alignments:
+        print(alignment)
